dc205d0145384e443cd33d253aab8282c3705f6d
DavidSoh[BR11] <mail@stdj.me>
2022-09-22 00:50:52 +0800

    bring back js export

--- act4-export.py
+++ act4-export.py
@@ -13,6 +13,8 @@ playlist_dict = dict()
 for enum_int, value_dict in enumerate(title_dict.values()):
     key_inner_str = "time{}".format(total_int-enum_int)
     value_inner_dict = {header_str:value_dict.get(header_str,"") for header_str in header_list}
+    value_inner_list = ["\"{}\": \"{}\"".format(header_str,value_dict.get(header_str,"")) for header_str in header_list]
+    value_inner_str = "\"time{}\":".format(total_int-enum_int) + "{\n" + ",\n".join(value_inner_list)
     tag_list = value_dict.get("tag",list())
     category_list = [n for n in value_dict.get("category",list()) if n[0] != "#"]
     tag_list.extend(sorted(list(set(category_list))))
@@ -22,6 +24,9 @@ for enum_int, value_dict in enumerate(title_dict.values()):
             deduplicate_tag_list.append(tag)
     value_inner_dict["tag"] = deduplicate_tag_list
     playlist_dict[key_inner_str] = value_inner_dict
+    value_inner_str = value_inner_str + ",\n\"tag\": {}".format(deduplicate_tag_list)
+    title_list.append(value_inner_str)
+outer_str = "const playlist = {\n"+"\n},\n".join(title_list)+"\n}\n};\n"
 
 with open("docs/blg-playlist.json",'w') as target_handler:
     json.dump(playlist_dict,target_handler,indent=0,sort_keys=True)
@@ -31,6 +36,8 @@ with open("docs/blg-playlist.toml",'w') as target_handler:
 print("    ----")
 print("    export docs/blg-tag_class")
 tag2class_dict = {tag_name: [str(n) for n in entry_detail["category"]] for tag_name, entry_detail in keyword_doc.items()}
+tag2class_list = ["\"{}\": {}".format(tag_name,tag_category_list) for tag_name, tag_category_list in tag2class_dict.items()]
+tag2class_str = "const tag_class = {\n"+",\n".join(tag2class_list)+"\n};\n"
 
 with open("docs/blg-tag_class.json",'w') as target_handler:
     json.dump(tag2class_dict,target_handler,indent=0,sort_keys=True)
@@ -45,10 +52,18 @@ for tag_name, entry_detail in keyword_doc.items():
         category_list = class2tag_dict.get(str(category_name),list())
         category_list.append(tag_name)
         class2tag_dict[str(category_name)] = category_list
+class2tag_list = list()
+for category_name, category_list in class2tag_dict.items():
+    class2tag_list.append("\"{}\": {}".format(category_name,category_list))
+class2tag_str = "const class_tag = {\n"+",\n".join(class2tag_list)+"\n};\n"
 
 with open("docs/blg-class_tag.json",'w') as target_handler:
     json.dump(class2tag_dict,target_handler,indent=0,sort_keys=True)
 with open("docs/blg-class_tag.toml",'w') as target_handler:
     tomlkit.dump(class2tag_dict,target_handler)
 
+with open("docs/blg-playlist.js",'w') as target_handler:
+    target_handler.write(outer_str)
+    target_handler.write(tag2class_str)
+    target_handler.write(class2tag_str)
 print("    ----\nEnd export")
